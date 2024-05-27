fd05c7ac10103cd467b34640674d949bede70bac
shinjiyun <ranji12@naver.com>
2024-05-27 15:27:09 +0900

    블로그 메인화면 js

---
+++ public/mainJs.js
@@ -0,0 +1,38 @@
+// const fs = require('fs');
+const root = document.getElementById("root");
+root.style.width="100vw";
+root.style.height="100vh";
+function elementCreate(parent, elementName, elementType,elementId){
+  elementName=document.createElement(elementType);
+  parent.appendChild(elementName);
+  elementName.id=elementId;
+}
+elementCreate(root, "blogTitle", "div","blogTitle");
+blogTitle.style.textAlign="center";
+blogTitle.style.marginTop="20px";
+blogTitle.innerHTML=`<h1 style="marin-top:20px"><a href="./" style="text-decoration:none; color:black;">나만의 블로그</a></h1>`;
+
+elementCreate(root, "blogContentDiv", "div","blogContentDiv");
+const blogContentDiv = document.getElementById("blogContentDiv");
+blogContentDiv.style.height="100%";
+blogContentDiv.style.display="grid";
+blogContentDiv.style.gridTemplateRows="repeat(4,1fr)"
+blogContentDiv.style.gridTemplateColumns="repeat(4,1fr)"
+elementCreate(blogContentDiv, "blogContent", "div", "blogContent");
+elementCreate(blogContentDiv, "blogleft", "div", "blogleft");
+elementCreate(blogContentDiv, "blogright", "div", "blogright");
+elementCreate(blogContentDiv, "blogfooter", "div", "blogfooter");
+
+const blogContent = document.getElementById("blogContent");
+blogContent.style.display="flex";
+blogContent.style.flexDirection="column";
+blogContent.style.justifyContent="center";
+blogContent.style.alignItems="center";
+
+elementCreate(blogContent, "writePage", "a", "writePage");
+writePage.href="write.html";
+writePage.innerHTML="글쓰기";
+
+// elementCreate(blogContent, "jsonData", "div", "jsonData");
+// const lool = JSON.parse(fs.readFileSync("data.json","utf-8"));
+// jsonData.innerHTML=lool;
