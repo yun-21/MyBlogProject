df5e211ffc7fa85c003485ac8ffc5a5821ea0638
shinjiyun <ranji12@naver.com>
2024-05-28 14:50:11 +0900

    초 표씨까지 작성해줌

--- public/todayDate.js
+++ public/todayDate.js
@@ -3,7 +3,10 @@ function todayDate() {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
-  let result = year + "-" + month + "-" + day;
+  const hour = date.getHours();
+  const min = date.getMinutes();
+  const sec = date.getSeconds();
+  let result = year + "-" + month + "-" + day + "-" + hour + "-" + min + "-" + sec;
   return result;
 }
 module.exports=todayDate;
