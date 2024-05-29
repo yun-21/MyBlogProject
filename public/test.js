a069372b3ae6496cca809883eeb20753274cf8a0
shinjiyun <ranji12@naver.com>
2024-05-29 16:32:47 +0900

    서버에 쓴 내용과 동일해서 쓰지 않을 가능성이 높음

--- public/test.js
+++ public/test.js
@@ -1,15 +1,19 @@
 const fs = require('fs');
+const todayDate = require('./todayDate');
 const jsonArr = fs.readdir("./public/data",(error,file)=>{
   for(let i=0; i<file.length; i++){
     const jsonData = JSON.parse(fs.readFileSync(`./public/data/${file[i]}`,"utf8"));
-    console.log(jsonData);
     for(let key in jsonData){
       if(key==="title"){
-        console.log(`<h1>${jsonData[key]}</h1>`);
+        var a = `<h1>${jsonData[key]}</h1>`;
       }
       else if(key==="content"){
-        console.log(`<h3>${jsonData[key]}</h3>`);
+        var b = `<h3>${jsonData[key]}</h3>`;
       }
     }
+    const all = a+b;
+    console.log(all);
+    fs.writeFileSync(`./public/dataHtml/${todayDate()}-data.html`, all, "utf-8");
   }
 });
+module.exports = jsonArr;
