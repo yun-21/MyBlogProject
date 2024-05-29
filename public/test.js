8f803570e74d5b16d291b94ef0b19aeb106a2547
shinjiyun <ranji12@naver.com>
2024-05-29 15:27:41 +0900

    파일리스트에 있는 파일을 불러와 키 값들을 빼낸 테스트

--- public/test.js
+++ public/test.js
@@ -1,14 +1,15 @@
 const fs = require('fs');
 const jsonArr = fs.readdir("./public/data",(error,file)=>{
   for(let i=0; i<file.length; i++){
-    console.log(fs.readFileSync(`./public/data/${file[i]}`,"utf8"));
+    const jsonData = JSON.parse(fs.readFileSync(`./public/data/${file[i]}`,"utf8"));
+    console.log(jsonData);
+    for(let key in jsonData){
+      if(key==="title"){
+        console.log(`<h1>${jsonData[key]}</h1>`);
+      }
+      else if(key==="content"){
+        console.log(`<h3>${jsonData[key]}</h3>`);
+      }
+    }
   }
-})
-// for(let key in jsonData){
-//   if(key==="title"){
-//     console.log(`<h1>${jsonData[key]}</h1>`);
-//   }
-//   else if(key==="content"){
-//     console.log(`<h3>${jsonData[key]}</h3>`);
-//   }
-// }
+});
